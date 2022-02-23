58c96cfe747e7feeb4e47fa4b826e8dd42ce33f4
MainLah <rendrakusumab@gmail.com>
2022-02-23 20:31:06 +0700

    Blurry Loading

---
+++ Blurry Loading/script.js
@@ -0,0 +1,18 @@
+const bg = document.querySelector(".bg");
+const loadPercent = document.querySelector("#load-percent");
+let index = 100;
+let percent = 0;
+
+const interval = setInterval(() => {
+  bg.style.filter = `blur(${index}px)`;
+  loadPercent.style.opacity = `${index / 100}`;
+  index--;
+  loadPercent.innerText = `${percent}%`;
+  percent++;
+  if (index === -1 && percent === 101) {
+    clearInterval(interval);
+    setTimeout(() => {
+      loadPercent.innerText = "";
+    }, 1000);
+  }
+}, 25);
