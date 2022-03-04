73846cf556a1f005086a92330d3df2ddff3f43d5
MainLah <rendrakusumab@gmail.com>
2022-03-04 17:31:43 +0700

    AKHIRNYA

--- Form Wave/script.js
+++ Form Wave/script.js
@@ -1,5 +1,6 @@
-const label = document.querySelectorAll(".label");
+const label = document.querySelectorAll("label");
 const input = document.querySelectorAll("input");
+// const span = document.querySelectorAll("label span");
 
 label.forEach((e) => {
   e.querySelectorAll("span").forEach((span, i) => {
@@ -7,10 +8,17 @@ label.forEach((e) => {
   });
 });
 
-input.forEach((e) => {
-  e.addEventListener("focus", () => {
-    label.forEach((el) =>
-      el.querySelectorAll("span").forEach((ele) => ele.classList.remove("down"))
-    );
+input.forEach((input, i) => {
+  input.addEventListener("focus", () => {
+    const span = label[i].querySelectorAll("span");
+    span.forEach((span) => {
+      span.classList.add("up");
+    });
+  });
+  input.addEventListener("blur", () => {
+    const span = label[i].querySelectorAll("span");
+    span.forEach((span) => {
+      span.classList.remove("up");
+    });
   });
 });
