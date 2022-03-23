4f60b637c93100c9a612b1ca7ad888a4dde2a5e6
MainLah <rendrakusumab@gmail.com>
2022-03-23 18:43:43 +0700

    finished drink water

---
+++ Drink Water/script.js
@@ -0,0 +1,19 @@
+const cups = document.querySelectorAll(".cups");
+const water = document.querySelector(".water");
+
+const heightCounter = 100 / cups.length;
+let currentIndex = 0;
+
+cups.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    if (index === 0) {
+      if (water.style.height === "12.5%") {
+        water.style.height = 0;
+        return;
+      }
+    }
+
+    water.style.height = `${heightCounter * (index + 1)}%`;
+    currentIndex = index;
+  });
+});
